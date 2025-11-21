b26dc91ef60e17bed98ea702a4dd0ac6b6f5a603
Joseph Seo <jseo305@gatech.edu>
2025-11-21 11:05:10 -0500

    Added Ally Cards

--- custom_event_resources/AssertIsEnemyOrAllyEvent.gd
+++ custom_event_resources/AssertIsEnemyOrAllyEvent.gd
@@ -1,10 +1,14 @@
 extends EventResource
 class_name AssertIsEnemyOrAllyEvent
 
-@export var event_to_do : EventResource
+@export var events_to_do : Array[EventResource]
 
 func on_execute() -> bool:
-	
+	if ((m_card_invoker.resource.type == CardResource.CardType.ENEMY or \
+		m_card_invoker.resource.type == CardResource.CardType.ALLY)):
+			# TODO: Can do this through Multi Events, just rely on required events instead
+			for event in events_to_do:
+				event.on_execute()
 	return false
 	
 func required_events() -> Array[EventResource]:
